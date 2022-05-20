640b0851eadd70e727a9cb62d049589c61384d6a
UltraMan <super-kolia2006@ukr.net>
2022-05-20 13:15:51 +0300

    main

--- app/src/main/java/com/example/notesv2/app/App.kt
+++ app/src/main/java/com/example/notesv2/app/App.kt
@@ -1,11 +1,7 @@
 package com.example.notesv2.app
 
 import android.app.Application
-import com.example.notesv2.data.db.AppDatabase
-import com.example.notesv2.data.repository.NoteRealization
-import com.example.notesv2.domain.repositories.NoteRepository
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application()
